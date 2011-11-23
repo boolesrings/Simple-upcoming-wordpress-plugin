6c619639d2c44a7774dcbde4f5918370e7cce15c
Samuel Coskey <scoskey@gmail.com>
2011-11-22 19:32:10 -0500

    added date_format and show_date

--- readme.txt
+++ readme.txt
@@ -2,21 +2,37 @@
 
 Contributors: sgcoskey
 Donate link: http://boolesrings.org
-Tags: events, event, upcoming
+Tags: events, event, upcoming, calendar
 Requires at least: 3.0
 Tested up to: 3.2.1
 Stable tag: 0.0
 
 Yet another plugin to make a list of upcoming events.  Just add an
 "EventDate" to any post, and then obtain a list of upcoming events
-using the [upcoming] tag.
+using the [upcoming] shortcode.
 
 == Description ==
 
-Some more info needed...
+This is another simple plugin to add a little functionality of a
+calendar to your blog.  With this plugin you can specify, for any of
+your posts, an associated "EventDate".  Then, elsewhere on your site,
+you can retrieve a list of future events using the [upcoming]
+shortcode.
+
+The [upcoming] shortcode supports several options:
+
+* show_excerpt: if defined, an excerpt of the post will be included below the
+post title
+
+* show_date: if defined, the date will precede the post title
+
+* date-format: if writing the date, do so using this php date format
+
+The output can then be further formatted using CSS.  We recommend the plugin
+"Improved Simpler CSS" for easy modification to the output!
 
 Fork this plugin on
-[GitHub](https://github.com/scoskey/Simple-Mathjax-wordpress-plugin)
+[GitHub](https://github.com/scoskey/Simple-upcoming-wordpress-plugin)
 
 == Installation ==
 
--- simple-upcoming.php
+++ simple-upcoming.php
@@ -1,64 +1,91 @@
 <?php
-  /*
-   * Plugin Name: Simple upcoming
-   * Description: Assign an event date to any post.  List your upcoming events using the shortcode [upcoming].
-   * Version: 0.0
-   * Author: Samuel Coskey, Victoria Gitman
-   * Author URI: http://boolesrings.org
-   */
+/*
+ * Plugin Name: Simple upcoming
+ * Description: Assign an event date to any post.  List your upcoming events using the shortcode [upcoming].
+ * Version: 0.0
+ * Author: Samuel Coskey, Victoria Gitman
+ * Author URI: http://boolesrings.org
+*/
 
-  // Usage example:
-  // [upcoming cat=talks]
+/*
+ * The shortcode which shows a list of future events.
+ * Usage example:
+ * [upcoming category_name="talks" show_excerpt="yeah"]
+*/
+add_shortcode( 'upcoming', 'upcoming_loop' );
+
 function upcoming_loop( $atts ) {
-  global $more;
-  global $post;
+	global $more;
+	global $post;
 
-  // Arguments to the shortcode
-  extract( shortcode_atts( array(
-				 'cat' => '',
-				 ), $atts ) );
+	// Arguments to the shortcode
+	extract( shortcode_atts(  array(
+        	'category_name' => '',
+		'show_date' => '',
+		'date_format' => 'F j, Y',
+		'show_excerpt' => ''
+	), $atts ) );
 
-  // query the database for the posts with EventDate
-  // query syntax:
-  // http://codex.wordpress.org/Class_Reference/WP_Query#Parameters
-  $query = "";
-  if ( $cat ) {
-    $query .= "cat=" . $cat . '&';
-  }
-  $query .= 'orderby=meta_value&meta_key=EventDate';
-  add_filter( 'posts_where', 'where_future' );
-  $query_results = new WP_Query($query);
-  remove_filter( 'posts_where', 'where_future' );
-  
-  // building the output
-  $ret_val = "<div class='upcoming'>";
-  while ($query_results->have_posts()) {
-    $query_results->the_post();
-    
-    $ret_val .= "<h2>";
-    $ret_val .= "<a href='" . get_permalink() . "'>";
-    $ret_val .= the_title( '', '', false);
-    $ret_val .= "</a>";
-    $ret_val .= "</h2>";
-    // Tell wordpress to respect the [more] tag:
-    $more = 0;
-    $ret_val .= "<div>" .apply_filters( 'the_content', get_the_content() ). "</div>";
-    echo get_post_meta($post->ID,'EventDate',true);
-  }
-  wp_reset_postdata();
-  $ret_val .= "</div>";
+	/*
+	 * query the database for the posts with EventDate in the future
+	 * query syntax: http://codex.wordpress.org/Class_Reference/WP_Query#Parameters
+	*/
+	$query = "";
+	if ( $category_name ) {
+		$query .= "category_name=" . $category_name . '&';
+	}
+	$query .= 'meta_key=EventDate&orderby=meta_value';
+	add_filter( 'posts_where', 'where_future' );
+	$query_results = new WP_Query($query);
+	remove_filter( 'posts_where', 'where_future' );
+	
+	// building the output
+	$ret_val = "<ul class='upcoming'>";
+	while ( $query_results->have_posts() ) {
+		$query_results->the_post();
+		$ret_val .='<li>';
+		if ( $show_date ) {
+			$ret_val .= "<span class='upcoming_date'>";
+			$ret_val .= date($date_format,
+					strtotime(get_post_meta($post->ID, 'EventDate',true)));
+			$ret_val .= "</span>";
+			$ret_val .= "<span class='upcoming_date_sep'>: </span>\n";
+		}
+		$ret_val .= "<span class='upcoming_title'><a href='" . get_permalink() . "'>";
+		$ret_val .= the_title( '', '', false);
+		$ret_val .= "</a></span>\n";
+	        if ( $show_excerpt ) {
+			$ret_val .= "<div>\n";
+			$more = 0; // Tell wordpress to respect the [more] tag for the next line:
+			$ret_val .= apply_filters( 'the_content', get_the_content() );
+			$ret_val .= "</div>\n";
+		}
+		$ret_val .="</li>";
+	}
+	wp_reset_postdata();
+	$ret_val .= "</ul>";
 
-  return $ret_val;
-  }
-
-// additional filter on the query
-// needed since we don't know what day it is
-// http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_where
-function where_future ($where) {
-  $where .= "AND STR_TO_DATE(meta_value,'%m/%d/%Y') >= CURDATE()";
-  return $where;
+	return $ret_val;
 }
 
-add_shortcode( 'upcoming', 'upcoming_loop' );
+/*
+ * additional filter on the query
+ * needed since we don't know what day it is
+ * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_where
+*/
+function where_future ($where) {
+	$where .= "AND STR_TO_DATE(meta_value,'%m/%d/%Y') >= CURDATE()";
+	return $where;
+}
+
+/*
+ * Load our default style sheet
+*/
+add_action( 'wp_print_styles', 'enqueue_my_styles' );
+function enqueue_my_styles() {
+	wp_register_style( 'simple-upcoming-styles',
+			plugins_url('simple-upcoming-styles.css', __FILE__) );
+	wp_enqueue_style( 'simple-upcoming-styles' );
+}
 
 ?>
